b9912ab9b7a84350e96b26de54b552d816651c25
lean-dro <72211521+lean-dro@users.noreply.github.com>
2022-08-07 13:03:15 -0300

    Correção no banco

--- src/models/Conexao.php
+++ src/models/Conexao.php
@@ -3,7 +3,7 @@
         public static function conectar()
         {
         $conexao = new PDO("mysql:host=localhost;
-                            dbname=bdmba", 
+                            dbname=bdnet", 
                             "root",
                             "");
         $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
